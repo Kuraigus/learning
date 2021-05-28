ab8ad3b105f047f81b51282d4610fbe5d40dbec0
kuraigus <kuraigus@hotmail.com>
2021-05-28 20:57:05 -0300

    aula1 completa

---
+++ curso_java/java-polimorfismo/aula1/src/TesteGerente.java
@@ -0,0 +1,19 @@
+public class TesteGerente {
+    public static void main(String[] args) {
+        Gerente joao = new Gerente();
+
+        joao.setNome("Joao Victor");
+        joao.setCpf("222.222.222-22");
+        joao.setSalario(5000.0);
+
+        System.out.println(joao.getNome());
+        System.out.println(joao.getCpf());
+        System.out.println(joao.getSalario());
+
+        joao.setSenha(2222);
+
+        boolean autenticou = joao.autentica(2222);
+
+        System.out.println(autenticou);
+    }
+}
