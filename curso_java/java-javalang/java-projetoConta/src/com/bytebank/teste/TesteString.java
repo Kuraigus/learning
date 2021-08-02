f4f1da9e17c5fabbbed2d91792d1167badaafa7a
Kuraigus <kuraigus@hotmail.com>
2021-08-01 23:34:43 -0300

    aula 4 completada

---
+++ curso_java/java-javalang/java-projetoConta/src/com/bytebank/teste/TesteString.java
@@ -0,0 +1,14 @@
+package com.bytebank.teste;
+
+public class TesteString {
+    public static void main(String[] args) {
+        String nome = "Alura"; // Object literal
+//        String nome = new String("Alura");
+
+
+        String resultado = nome.replace("A", "a");
+
+        System.out.println(resultado);
+
+    }
+}
