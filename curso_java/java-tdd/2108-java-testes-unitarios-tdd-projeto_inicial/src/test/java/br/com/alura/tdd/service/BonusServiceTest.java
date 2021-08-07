91011bed1e12cbb9a84cb19b630db6d24411fcba
Kuraigus <kuraigus@hotmail.com>
2021-08-07 03:40:39 -0300

    aula 4 completada

--- curso_java/java-tdd/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ curso_java/java-tdd/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -7,16 +7,21 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
-
-        assertEquals(BigDecimal.ZERO, bonus);
+//        assertThrows(IllegalArgumentException.class,
+//                () -> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
+        try {
+            service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+            fail("Nao lancou Exception");
+        } catch(Exception e) {
+            assertEquals("Funcionario com salario maior que 10000", e.getMessage());
+        }
     }
 
     @Test
