d5430cb3e938c9758c934fbfb03b2a6dbfe5da73
Kuraigus <kuraigus@hotmail.com>
2021-08-06 23:28:19 -0300

    Aula 2 completada

--- curso_java/java-moderno/java8/src/OrdenaStrings.java
+++ curso_java/java-moderno/java8/src/OrdenaStrings.java
@@ -12,29 +12,12 @@ public class OrdenaStrings {
         palavras.add("editora casa do codigo");
         palavras.add("caelum");
 
-        Comparator<String> comparador = new ComparadorDeTamanhos();
+//        Comparator<String> comparador = new ComparadorDeTamanhos();
 //        Collections.sort(palavras, comparador);
-        palavras.sort(comparador);
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 
 //        System.out.println(palavras);
 
-        Consumer<String> consumidor = new ImprimeNaLinha();
-        palavras.forEach(consumidor);
-    }
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-    @Override
-    public void accept(String s) {
-        System.out.println(s);
-    }
-}
-
-class ComparadorDeTamanhos implements Comparator<String> {
-
-    @Override
-    public int compare(String s1, String s2) {
-        return s1.length() - s2.length();
+        palavras.forEach(s -> System.out.println(s));
     }
 }
